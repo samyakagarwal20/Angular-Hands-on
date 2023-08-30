f7fa8c20fd114bb2f8e497a6fccc569b04c10a7e
samyak <samyakagarwal20@gmail.com>
2023-08-30 20:00:39 +0530

    experimenting with @Component properties

--- demo-app/src/app/servers/servers.component.ts
+++ demo-app/src/app/servers/servers.component.ts
@@ -2,7 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  // selector: '[app-server]',
+  // selector: '.app-server',
   templateUrl: './servers.component.html',
+  template: `
+  <app-server></app-server>
+  <app-server></app-server>
+  `,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
